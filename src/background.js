465f927e156631cf053a31f81195abc6e814ee45
David Moreira <david@multiplier.com.br>
2022-03-10 17:18:46 -0300

    feat: configura backend

--- src/background.js
+++ src/background.js
@@ -5,6 +5,9 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+// AUTOPLAY BACKEND
+require('../backend/app.js')
+
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
